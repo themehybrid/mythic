8b132451c356ea43a2c41543f518b13d24dbc6a7
Justin Tadlock <justintadlock@gmail.com>
2018-02-17 08:13:27 -0600

    Add Hybrid Core as a dependency for now.  We can decide later if we want to continue with it as part of the starter theme.

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@ namespace ABC;
 
 require_once( trailingslashit( get_template_directory() ) . 'vendor/autoload.php' );
 
+define( 'HYBRID_DIR', trailingslashit( get_parent_theme_file_path( 'vendor/justintadlock/hybrid-core' ) ) );
+define( 'HYBRID_URI', trailingslashit( get_parent_theme_file_uri(  'vendor/justintadlock/hybrid-core' ) ) );
+
+require_once( HYBRID_DIR . 'hybrid.php' );
+
 require_once( get_parent_theme_file_path( 'bootstrap/app.php' ) );
 
 function templates() {
