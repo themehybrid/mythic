5f20b610138deb03dfab6ef0a6f226481e7b61c2
Justin Tadlock <justintadlock@gmail.com>
2018-02-20 13:05:30 -0600

    Add inline documentation for the `View` class.

--- app/class-view.php
+++ app/class-view.php
@@ -4,11 +4,55 @@ namespace ABC;
 
 class View {
 
+	/**
+	 * Name of the view. This is primarily used as the folder name. However,
+	 * it can also be the filename as the finall fallback if no folder exists.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
 	protected $name = '';
+
+	/**
+	 * Array of slugs to look for. This creates the hierarchy based on the
+	 * `$name` property (e.g., `{$name}/{$slug}.php`). Slugs are used in
+	 * the order that they are set.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
 	protected $slugs = [];
+
+	/**
+	 * An array of data that is passed into the view template.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    array
+	 */
 	protected $data = [];
+
+	/**
+	 * The template filename.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
 	protected $template = '';
 
+	/**
+	 * Sets up the view properties.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  string  $name
+	 * @param  array   $slugs
+	 * @param  array   $data
+	 * @return object
+	 */
 	public function __construct( $name, $slugs = [], $data = [] ) {
 
 		$this->name  = $name;
@@ -16,28 +60,53 @@ class View {
 		$this->data  = (array) apply_filters( app()->namespace . "/view_data_{$this->name}",  $data  );
 	}
 
+	/**
+	 * Locates the template using the core WordPress `locate_template()` function.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
 	protected function locate() {
 
 		$this->template = locate_template( $this->get_hierarchy(), false, false );
 	}
 
+	/**
+	 * Uses the array of template slugs to build a hierarchy of potential
+	 * templates that can be used.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return array
+	 */
 	protected function get_hierarchy() {
 
+		// Uses the slugs to build a hierarchy.
 		foreach ( $this->slugs as $slug ) {
 
 			$templates[] = "resources/views/{$this->name}/{$slug}.php";
 		}
 
+		// Add in a `default.php` template.
 		if ( ! in_array( 'default', $this->slugs ) ) {
 
 			$templates[] = "resources/views/{$this->name}/default.php";
 		}
 
+		// Fallback to `{$name}.php` as a last resort.
 		$templates[] = "resources/views/{$this->name}.php";
 
 		return apply_filters( app()->namespace . "/view_hierarchy_{$this->name}", $templates );
 	}
 
+	/**
+	 * Sets up data to be passed to the template and renders it.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
 	public function render() {
 
 		// Locate the template.
@@ -59,6 +128,13 @@ class View {
 		}
 	}
 
+	/**
+	 * Returns the template output as a string.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string
+	 */
 	public function fetch() {
 
 		ob_start();
