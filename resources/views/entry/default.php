09805d96191174347444976c6e41d6d3605a4173
Justin Tadlock <justintadlock@gmail.com>
2018-02-22 12:33:37 -0600

    Move code from `default.php` to `post.php`.  Remove some of the `post` post type stuff from `default.php`.

--- resources/views/entry/default.php
+++ resources/views/entry/default.php
@@ -4,14 +4,6 @@
 
 		<header class="entry__header">
 			<h1 class="entry__title"><?php single_post_title() ?></h1>
-
-			<div class="entry__byline">
-				<?php hybrid_post_author( array( 'wrap' => '<span class="entry__author">%2$s</span>' ) ) ?>
-				<span class="sep"><?php _ex( '&middot;', 'meta separator' ) ?></span>
-				<time class="entry__published"><?php echo get_the_date() ?></time>
-				<span class="sep"><?php _ex( '&middot;', 'meta separator' ) ?></span>
-				<?php comments_popup_link( false, false, false, 'entry__comments-link' ) ?>
-			</div>
 		</header>
 
 		<div class="entry__content">
@@ -19,28 +11,14 @@
 			<?php wp_link_pages() ?>
 		</div>
 
-		<footer class="entry__foooter">
-			<?php hybrid_post_terms( array( 'taxonomy' => 'category' ) ) ?>
-			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag' ) ) ?>
-		</footer>
-
 	<?php else : ?>
 
 		<header class="entry__header">
 			<h2 class="entry__title"><a href="<?php the_permalink() ?>"><?php the_title() ?></h2>
-
-			<div class="entry__byline">
-				<?php hybrid_post_author( array( 'wrap' => '<span class="entry__author">%2$s</span>' ) ) ?>
-				<span class="sep"><?php _ex( '&middot;', 'meta separator' ) ?></span>
-				<time class="entry__published"><?php echo get_the_date() ?></time>
-				<span class="sep"><?php _ex( '&middot;', 'meta separator' ) ?></span>
-				<?php comments_popup_link( false, false, false, 'entry__comments-link' ) ?>
-			</div>
 		</header>
 
 		<div class="entry__summary">
 			<?php the_excerpt() ?>
-			<?php wp_link_pages() ?>
 		</div>
 
 	<?php endif ?>
