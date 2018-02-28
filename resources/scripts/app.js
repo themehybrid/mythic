eb2c246d636c5cc9ca1e6053ea7dbbfaf4554ff0
Justin Tadlock <justintadlock@gmail.com>
2018-02-28 13:07:50 -0600

    Add default `app.js` and `/resources/scripts` folder.

---
+++ resources/scripts/app.js
@@ -0,0 +1,26 @@
+/**
+ * Primary script.
+ *
+ * Primary JavaScript file. Any includes or anything imported should
+ * be filtered through this file and eventually saved back into the
+ * `/resources/dist/js/app.min.js` file.
+ *
+ * @package   ABC
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2018, Justin Tadlock
+ * @link      https://themehybrid.com/themes/abc
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/**
+ * A simple immediately-invoked function expression to kick-start
+ * things and encapsulate our code.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+(function () {
+
+	// Do stuff.
+})();
