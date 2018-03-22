f5e17d083271f07e46b2d17ad4b555df07f3b886
Justin Tadlock <justintadlock@gmail.com>
2018-03-22 07:39:49 -0500

    First pass at a `content/404.php` template. We can change the content later.

---
+++ resources/views/content/404.php
@@ -0,0 +1,15 @@
+<main class="app-main">
+
+	<div class="entry entry--error">
+
+		<h1 class="entry__title"><?php esc_html_e( 'Whoah, partner!' ) ?></h1>
+
+		<div class="entry__content">
+			<p><?php esc_html_e( 'It looks like you stumbled upon a page that does not exist. Perhaps rolling the dice with a search might help.' ) ?></p>
+
+			<?php get_search_form() ?>
+		</div>
+
+	</div>
+
+</main>
