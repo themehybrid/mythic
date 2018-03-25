9478829d08b4352ee2f0fa3545674aa86ffc71ea
Justin Tadlock <justintadlock@gmail.com>
2018-03-25 11:42:38 -0500

    Correct code in `comment/ping.php`.

--- resources/views/comment/ping.php
+++ resources/views/comment/ping.php
@@ -1,11 +1,9 @@
-<?php namespace ABC; ?>
-
-<li <?php hybrid_attr( 'comment' ) ?>>
+<li <?php Hybrid\attr( 'comment' ) ?>>
 
 	<div class="thread__meta">
 		<span class="thread__author"><?php comment_author_link() ?></span><br />
 		<a href="<?php comment_link() ?>" class="comment__permalink"><time class="comment__published"><?php printf( __( '%s ago' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ) ?></time></a>
-		<?php edit_comment_link( null, get_meta_sep() ) ?>
+		<?php edit_comment_link( null, ABC\get_meta_sep() ) ?>
 	</div>
 
 <?php /* No closing </li> is needed.  WordPress will know where to add it. */ ?>
