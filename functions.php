d852c7134cf4a19756a41f8c2343a32f99b8a132
Justin Tadlock <justintadlock@gmail.com>
2018-04-03 16:18:12 -0500

    Remove the `bootstrap-theme.php` file and put the bootstrap process into the normal theme `functions.php` with instructions on how to set up a more robust bootstrapping process.

--- functions.php
+++ functions.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Theme functions.
+ * Theme functions file.
  *
- * This file is used to load the autoload and bootstrap files necessary
- * for kick-starting the theme.
+ * This file is used to bootstrap the theme.
  *
  * @package   ABC
  * @author    Justin Tadlock <justintadlock@gmail.com>
@@ -12,12 +11,22 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-// Auto-load any projects via the Composer autoloader. Be sure to check if the
-// file exists in case someone's using Composer to load their dependencies in
-// a different directory.
+# Auto-load any projects via the Composer autoloader. Be sure to check if the
+# file exists in case someone's using Composer to load their dependencies in
+# a different directory.
 if ( file_exists( get_parent_theme_file_path( 'vendor/autoload.php' ) ) ) {
         require_once( get_parent_theme_file_path( 'vendor/autoload.php' ) );
 }
 
-// Bootstrap the theme.
-require_once( get_parent_theme_file_path( 'app/bootstrap-theme.php' ) );
+# Bootstrap the theme. This loads any functions-files from the `/app` folder
+# that are needed. If the theme needs a more robust bootstrapping process, it's
+# recommended to create a `/bootstrap` folder and load those files directly.
+array_map(
+	function( $file ) {
+		require_once( get_parent_theme_file_path( "app/{$file}.php" ) );
+	},
+	[
+		'functions-setup',
+		'functions-template'
+	]
+);
