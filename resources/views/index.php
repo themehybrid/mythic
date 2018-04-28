a9e587ea7ad5118374cff34fbb152daa54f8ed51
Justin Tadlock <justintadlock@gmail.com>
2018-04-28 17:15:38 -0500

    Use the `get_global_hierarchy()` function from Hybrid.

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,10 +1,10 @@
 <?php
 
 // Load header/* template.
-Hybrid\render_view( 'header', Hybrid\get_template_hierarchy() );
+Hybrid\render_view( 'header', Hybrid\get_global_hierarchy() );
 
 // Load content/* template.
-Hybrid\render_view( 'content', Hybrid\get_template_hierarchy() );
+Hybrid\render_view( 'content', Hybrid\get_global_hierarchy() );
 
 // Load footer/* template.
-Hybrid\render_view( 'footer', Hybrid\get_template_hierarchy() );
+Hybrid\render_view( 'footer', Hybrid\get_global_hierarchy() );
