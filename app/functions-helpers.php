74c9d51cff58a1a66b8445a6fa93759b1aa11e1b
Justin Tadlock <justintadlock@gmail.com>
2018-06-15 16:56:24 -0500

    Split the `mix()` function away from `asset()`.  Each function should do one thing and do that one thing well.  Also, this should make it easier for theme authors to remove for WordPress.org if they need to.

--- app/functions-helpers.php
+++ app/functions-helpers.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Theme setup functions.
+ * Helper functions.
  *
  * This file holds basic helper functions used within the theme.
  *
@@ -18,16 +18,10 @@ use function Hybrid\app;
 
 /**
  * Helper function for outputting an asset URL in the theme. This integrates
- * with Laravel Mix for handling cache busting. If used when enqueue a script or
- * style, it'll append an ID to the file name in a production build.
+ * with Laravel Mix for handling cache busting. If used when you enqueue a script
+ * or style, it'll append an ID to the file name in a production build.
  *
- * Note that `file_get_contents()` is not allowed on WordPress.org. If building
- * a theme for the WP directory, you'll need to remove that bit of the code.
- *
- * @link https://laravel.com/docs/5.6/mix#versioning-and-cache-busting
- * @link https://github.com/WordPress/theme-check/issues/55
- * @link https://wordpress.stackexchange.com/questions/166161/why-cant-the-wp-filesystem-api-read-googlefonts-json/166175
- *
+ * @link   https://laravel.com/docs/5.6/mix#versioning-and-cache-busting
  * @since  1.0.0
  * @access public
  * @param  string  $path
@@ -35,10 +29,35 @@ use function Hybrid\app;
  */
 function asset( $path ) {
 
-	$manifest = app( 'abc/mix' );
+	// Get the Laravel Mix manifest.
+	$manifest = mix();
 
 	// Make sure to trim any slashes from the front of the path.
 	$path = '/' . ltrim( $path, '/' );
+
+	if ( $manifest && isset( $manifest[ $path ] ) ) {
+		$path = $manifest[ $path ];
+	}
+
+	return get_theme_file_uri( 'dist' . $path );
+}
+
+/**
+ * Returns the Laravel Mix manifest.
+ *
+ * Note that `file_get_contents()` is not allowed on WordPress.org. If building
+ * a theme for the WP directory, you'll need to remove this function and the
+ * reference to it in the `asset()` function.
+ *
+ * @link   https://github.com/WordPress/theme-check/issues/55
+ * @link   https://wordpress.stackexchange.com/questions/166161/why-cant-the-wp-filesystem-api-read-googlefonts-json/166175
+ *
+ * @since  1.0.0
+ * @access public
+ * @return array|false
+ */
+function mix() {
+	$manifest = app( 'abc/mix' );
 
 	// If there is no manifest saved yet, let's see if we can find one.
 	if ( ! $manifest ) {
@@ -54,9 +73,5 @@ function asset( $path ) {
 		}
 	}
 
-	if ( $manifest && isset( $manifest[ $path ] ) ) {
-		$path = $manifest[ $path ];
-	}
-
-	return get_theme_file_uri( 'dist' . $path );
+	return $manifest;
 }
