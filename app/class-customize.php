9f199b8e79587c52b69cf103860e62ffcee8575e
Justin Tadlock <justintadlock@gmail.com>
2018-06-24 06:49:03 -0500

    Tabs, not spaces.

--- app/class-customize.php
+++ app/class-customize.php
@@ -26,146 +26,146 @@ use WP_Customize_Manager;
  */
 class Customize {
 
-        /**
-         * Adds actions on the appropriate customize action hooks.
-         *
-         * @since  1.0.0
-         * @access public
-         * @return void
-         */
-        public function __construct() {
+	/**
+	 * Adds actions on the appropriate customize action hooks.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
 
-                // Register panels, sections, settings, controls, and partials.
-                add_action( 'customize_register', [ $this, 'registerPanels'   ] );
-                add_action( 'customize_register', [ $this, 'registerSections' ] );
-                add_action( 'customize_register', [ $this, 'registerSettings' ] );
-                add_action( 'customize_register', [ $this, 'registerControls' ] );
-                add_action( 'customize_register', [ $this, 'registerPartials' ] );
+		// Register panels, sections, settings, controls, and partials.
+		add_action( 'customize_register', [ $this, 'registerPanels'   ] );
+		add_action( 'customize_register', [ $this, 'registerSections' ] );
+		add_action( 'customize_register', [ $this, 'registerSettings' ] );
+		add_action( 'customize_register', [ $this, 'registerControls' ] );
+		add_action( 'customize_register', [ $this, 'registerPartials' ] );
 
-                // Enqueue scripts and styles.
-                add_action( 'customize_controls_enqueue_scripts', [ $this, 'controlsEnqueue'] );
-                add_action( 'customize_preview_init', [ $this, 'previewEnqueue' ] );
-        }
+		// Enqueue scripts and styles.
+		add_action( 'customize_controls_enqueue_scripts', [ $this, 'controlsEnqueue'] );
+		add_action( 'customize_preview_init', [ $this, 'previewEnqueue' ] );
+	}
 
-        /**
-         * Callback for registering panels.
-         *
-         * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#panels
-         * @since  1.0.0
-         * @access public
-         * @param  WP_Customize_Manager  $manager
-         * @return void
-         */
-        public function registerPanels( WP_Customize_Manager $manager ) {}
+	/**
+	 * Callback for registering panels.
+	 *
+	 * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#panels
+	 * @since  1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerPanels( WP_Customize_Manager $manager ) {}
 
-        /**
-         * Callback for registering sections.
-         *
-         * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#sections
-         * @since  1.0.0
-         * @access public
-         * @param  WP_Customize_Manager  $manager
-         * @return void
-         */
-        public function registerSections( WP_Customize_Manager $manager ) {}
+	/**
+	 * Callback for registering sections.
+	 *
+	 * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#sections
+	 * @since  1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerSections( WP_Customize_Manager $manager ) {}
 
-        /**
-         * Callback for registering settings.
-         *
-         * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#settings
-         * @since  1.0.0
-         * @access public
-         * @param  WP_Customize_Manager  $manager
-         * @return void
-         */
-        public function registerSettings( WP_Customize_Manager $manager ) {
+	/**
+	 * Callback for registering settings.
+	 *
+	 * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#settings
+	 * @since  1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerSettings( WP_Customize_Manager $manager ) {
 
-                // Update the `transform` property of core WP settings.
-                $settings = [
-                        $manager->get_setting( 'blogname' ),
-                        $manager->get_setting( 'blogdescription' ),
-                        $manager->get_setting( 'header_textcolor' ),
-                        $manager->get_setting( 'header_image' )
-                ];
+		// Update the `transform` property of core WP settings.
+		$settings = [
+			$manager->get_setting( 'blogname' ),
+			$manager->get_setting( 'blogdescription' ),
+			$manager->get_setting( 'header_textcolor' ),
+			$manager->get_setting( 'header_image' )
+		];
 
-                array_walk( $settings, function( &$setting ) {
-                        $setting->transport = 'postMessage';
-                } );
-        }
+		array_walk( $settings, function( &$setting ) {
+			$setting->transport = 'postMessage';
+		} );
+	}
 
-        /**
-         * Callback for registering controls.
-         *
-         * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#controls
-         * @since  1.0.0
-         * @access public
-         * @param  WP_Customize_Manager  $manager
-         * @return void
-         */
-        public function registerControls( WP_Customize_Manager $manager ) {}
+	/**
+	 * Callback for registering controls.
+	 *
+	 * @link   https://developer.wordpress.org/themes/customize-api/customizer-objects/#controls
+	 * @since  1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerControls( WP_Customize_Manager $manager ) {}
 
-        /**
-         * Callback for registering partials.
-         *
-         * @link   https://developer.wordpress.org/themes/customize-api/tools-for-improved-user-experience/#selective-refresh-fast-accurate-updates
-         * @since  1.0.0
-         * @access public
-         * @param  WP_Customize_Manager  $manager
-         * @return void
-         */
-        public function registerPartials( WP_Customize_Manager $manager ) {
+	/**
+	 * Callback for registering partials.
+	 *
+	 * @link   https://developer.wordpress.org/themes/customize-api/tools-for-improved-user-experience/#selective-refresh-fast-accurate-updates
+	 * @since  1.0.0
+	 * @access public
+	 * @param  WP_Customize_Manager  $manager
+	 * @return void
+	 */
+	public function registerPartials( WP_Customize_Manager $manager ) {
 
-                // If the selective refresh component is not available, bail.
-                if ( ! isset( $manager->selective_refresh ) ) {
-                        return;
-                }
+		// If the selective refresh component is not available, bail.
+		if ( ! isset( $manager->selective_refresh ) ) {
+			return;
+		}
 
-                // Selectively refreshes the title in the header when the core
-                // WP `blogname` setting changes.
-                $manager->selective_refresh->add_partial( 'blogname', [
-                        'selector'        => '.app-header__title a',
-                        'render_callback' => function() {
-                                return get_bloginfo( 'name', 'display' );
-                        }
-                ] );
+		// Selectively refreshes the title in the header when the core
+		// WP `blogname` setting changes.
+		$manager->selective_refresh->add_partial( 'blogname', [
+			'selector'	=> '.app-header__title a',
+			'render_callback' => function() {
+				return get_bloginfo( 'name', 'display' );
+			}
+		] );
 
-                // Selectively refreshes the description in the header when the
-                // core WP `blogdescription` setting changes.
-                $manager->selective_refresh->add_partial( 'blogdescription', [
-                        'selector'        => '.app-header__description',
-                        'render_callback' => function() {
-                                return get_bloginfo( 'description', 'display' );
-                        }
-                ] );
-        }
+		// Selectively refreshes the description in the header when the
+		// core WP `blogdescription` setting changes.
+		$manager->selective_refresh->add_partial( 'blogdescription', [
+			'selector'	=> '.app-header__description',
+			'render_callback' => function() {
+				return get_bloginfo( 'description', 'display' );
+			}
+		] );
+	}
 
-        /**
-         * Register or enqueue scripts/styles for the controls that are output
-         * in the controls frame. Note that if you have scripts/styles that are
-         * only needed for specific controls, you should register those here and
-         * enqueue them via the `enqueue()` method of your custom control class.
-         *
-         * @since  1.0.0
-         * @access public
-         * @return void
-         */
-        public function controlsEnqueue() {}
+	/**
+	 * Register or enqueue scripts/styles for the controls that are output
+	 * in the controls frame. Note that if you have scripts/styles that are
+	 * only needed for specific controls, you should register those here and
+	 * enqueue them via the `enqueue()` method of your custom control class.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function controlsEnqueue() {}
 
-        /**
-         * Register or enqueue scripts/styles for the live preview frame.
-         *
-         * @since  1.0.0
-         * @access public
-         * @return void
-         */
-        public function previewEnqueue() {
+	/**
+	 * Register or enqueue scripts/styles for the live preview frame.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function previewEnqueue() {
 
-                wp_enqueue_script(
-                        'abc-customize-preview',
-                        asset( 'scripts/customize-preview.js' ),
-                        [ 'customize-preview' ],
-                        false,
-                        true
-                );
-        }
+		wp_enqueue_script(
+			'abc-customize-preview',
+			asset( 'scripts/customize-preview.js' ),
+			[ 'customize-preview' ],
+			false,
+			true
+		);
+	}
 }
