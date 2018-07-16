6936439dcb17192e9f8cffe6b8f99f7f79b688c9
Justin Tadlock <justintadlock@gmail.com>
2018-07-16 11:20:24 -0500

    Adds in a compat file to take over the bootstrapping process if the theme doesn't meet the WP 4.9+ or PHP 5.6+ minimum requirements.

---
+++ app/bootstrap-compat.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * WP and PHP compatibility.
+ *
+ * Functions used to gracefully fail when a theme doesn't meet the minimum WP or
+ * PHP versions required. Note that only code that will work on PHP 5.2.4 should
+ * go into this file. Otherwise, it'll break on sites not meeting the minimum
+ * PHP requirement. Only call this file after initially checking that the site
+ * doesn't meet either the WP or PHP requirement.
+ *
+ * @package    ABC
+ * @subpackage Includes
+ * @author     Justin Tadlock <justintadlock@gmail.com>
+ * @copyright  Copyright (c) 2018, Justin Tadlock
+ * @link       https://themehybrid.com/themes/abc
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+# Add actions to fail at certain points in the load process.
+add_action( 'after_switch_theme', 'abc_switch_theme'   );
+add_action( 'load-customize.php', 'abc_load_customize' );
+add_action( 'template_redirect',  'abc_preview'        );
+
+/**
+ * Returns the compatibility messaged based on whether the WP or PHP minimum
+ * requirement wasn't met.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function abc_compat_message() {
+
+	if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) ) {
+
+		return sprintf(
+			__( 'ABC requires at least WordPress version %1$s. You are running version %2$s. Please upgrade and try again.' ),
+			'4.9',
+			$GLOBALS['wp_version']
+		);
+
+	} elseif ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+
+		return sprintf(
+			__( 'ABC requires at least PHP version %1$s. You are running version %2$s. Please upgrade and try again.' ),
+			'5.6',
+			PHP_VERSION
+		);
+	}
+
+	return '';
+}
+
+/**
+ * Switches to the previously active theme after the theme has been activated.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  string  $old_name
+ * @return void
+ */
+function abc_switch_theme( $old_name ) {
+
+	switch_theme( $old_name ? $old_name : WP_DEFAULT_THEME );
+
+	unset( $_GET['activated'] );
+
+	add_action( 'admin_notices', 'abc_upgrade_notice' );
+}
+
+/**
+ * Outputs an admin notice with the compatibility issue.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function abc_upgrade_notice() {
+
+	printf( '<div class="error"><p>%s</p></div>', esc_html( abc_compat_message() ) );
+}
+
+/**
+ * Kills the loading of the customizer.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  string  $old_name
+ * @return void
+ */
+function abc_load_customize() {
+
+	wp_die( esc_html( abc_compat_message() ), '', array( 'back_link' => true ) );
+}
+
+/**
+ * Kills the customizer previewer on installs prior to WP 4.7.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+function abc_preview() {
+
+	if ( isset( $_GET['preview'] ) ) {
+		wp_die( esc_html( abc_compat_message() ) );
+	}
+}
