d7442946da15cddee195f8062c6aa6a7ca503b3b
Justin Tadlock <justintadlock@gmail.com>
2018-09-12 14:15:37 -0500

    Use an `h3` tag for the comments pagination title. Fixes: https://github.com/justintadlock/mythic/issues/17

--- resources/views/nav/pagination/comments.php
+++ resources/views/nav/pagination/comments.php
@@ -1,5 +1,6 @@
 <?php Hybrid\Pagination\display( 'comments', [
 	'prev_text'  => __( '&larr; Previous' ),
 	'next_text'  => __( 'Next &rarr;' ),
-	'title_text' => __( 'Comments Navigation' )
+	'title_text' => __( 'Comments Navigation' ),
+	'title_tag'  => 'h3'
 ] );
