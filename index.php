d29a2fbc8f1ee954d2f35fee77561b5f1832a727
Justin Tadlock <justintadlock@gmail.com>
2018-10-22 10:14:12 -0500

    Fix typo in `index.php`. Fixes: https://github.com/justintadlock/mythic/pull/57
    Props @peiche

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  * This template should never be loaded on a site unless a plugin is doing
  * something that it shouldn't be doing or something weird is going on. We're
- * leaving the below code as an absolue fallback in case this file is loaded.
+ * leaving the below code as an absolute fallback in case this file is loaded.
  * All it does is correctly load up our `resources/views/index.php` template.
  */
 
